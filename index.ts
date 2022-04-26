1996dc02f35a3c1e3d4a3b2c0907bb1bdc449588
Wuilliam Lacruz <wuilliam321@gmail.com>
2022-04-25 22:06:50 -0300

    setup start

--- index.ts
+++ index.ts
@@ -6,6 +6,24 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+// [] health check
+// Products
+// [] GET /products
+// [] POST /products
+// [] PUT /products/:id
+// [] PATCH /products/:id
+// [] DELETE /products/:id
+//
+// Inventory
+// [] GET /inventory/:productId
+// [] PATCH /products/:id
+//
+// Orders
+// [] GET /orders
+// [] POST /orders
+// [] PUT /orders/:id
+// [] PATCH /orders/:id
+// [] DELETE /orders/:id
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
